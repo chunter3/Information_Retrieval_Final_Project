ad2e2ac0cafcb31047d15895c29b0d304e7fe6cb
Chris Hunter <chunter3@hawk.iit.edu>
2022-04-05 12:08:50 -0500

    Finished wiki crawler for now

---
+++ IR_System/IR_System/spiders/wiki_crawler.py
@@ -0,0 +1,27 @@
+import scrapy
+import requests
+
+class WikiCrawler(scrapy.Spider):
+
+    name = "wiki_crawler"
+
+    allowed_domains = ['en.wikipedia.org']
+
+    start_urls = ['https://en.wikipedia.org/wiki/Etrian_Odyssey']  
+
+    def parse(self, response):
+
+        # I've decided that only links w/in p or dl tags are relevant to crawl
+
+        rel_p_links = [(href,title) for href,title in zip([href for href in response.css('p a::attr(href)').getall() if "cite_note" not in href], response.css('p a::attr(title)').getall())]
+
+        rel_dl_links = [(href,title) for href,title in zip([href for href in response.css('dl a::attr(href)').getall() if "cite_note" not in href], response.css('dl a::attr(title)').getall())]  
+
+        for href,title in [*rel_p_links, *rel_dl_links]:
+            
+            webpage_content = requests.get(f"http://en.wikipedia.org{href}")
+             
+            filename = f'{title}.html' 
+            with open(filename, 'wb') as f:
+                f.write(webpage_content.content)
+
